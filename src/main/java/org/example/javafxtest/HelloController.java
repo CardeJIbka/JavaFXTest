a7d230c986adbf41ec459e3191ab0ddf8ae683d9
Voldmag <artemvoldmag@gmail.com>
2025-03-24 20:06:46 +0600

    Update HelloController.java

--- src/main/java/org/example/javafxtest/HelloController.java
+++ src/main/java/org/example/javafxtest/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("очень крутая игра");
+        welcomeText.setText("WWочень крутая играWW");
     }
 }
