86b8c975f6750fce7f3a92091bffb74d29f73796
Voldmag <artemvoldmag@gmail.com>
2025-03-26 22:53:56 +0600

    Update GameController.java

--- src/main/java/org/example/javafxtest/GameController.java
+++ src/main/java/org/example/javafxtest/GameController.java
@@ -1,5 +1,6 @@
 package org.example.javafxtest;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -13,15 +14,28 @@ public class GameController { // этот класс управляет свой
     private Button play; // говорим что есть кнопка с айди play
 
     @FXML
+    private Button exit; // говорим, что есть кнопка с id exit
+
+    @FXML
     void onPlayBtnClick(ActionEvent event) { // метод, который выполняется при нажатии кнопки "играть"
         play.setText("Играем..."); // изменить текст кнопки играть
         options.setText("Настройки"); // изменить текст кнопки настройки
+        exit.setText("Выйти"); // изменить текст кнопки выйти
     }
 
     @FXML
     void onOptionsBtnClick(ActionEvent event) { // метод, который выполняется при нажатии кнопки "настройки"
         options.setText("Настраиваем..."); // изменить текст кнопки настройки
         play.setText("Играть"); // изменить текст кнопки играть
+        exit.setText("Выйти"); // изменить текст кнопки выйти
+    }
+
+    @FXML
+    void onExitBtnClick(ActionEvent event) { // метод, который выполняется при нажатии кнопки "настройки"
+        Platform.exit(); // закрываем, если не сработает можно System.exit(0) тыкнуть
+        options.setText("Настройки"); // изменить текст кнопки настройки
+        play.setText("Играть"); // изменить текст кнопки играть
+        exit.setText("Выйти"); // изменить текст кнопки выйти
     }
 
 }
